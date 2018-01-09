0ba527600443b541fafbb1176911ed60acdd047b
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-01-09 16:49:17 +0100

    setup .plist for tests

---
+++ Support/AdvancedOperation.h
@@ -0,0 +1,19 @@
+//
+//  AdvancedOperation.h
+//  AdvancedOperation
+//
+//  Created by Alessandro Marzoli on 17/10/17.
+//  Copyright © 2017 Tinrobots. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for AdvancedOperation.
+FOUNDATION_EXPORT double AdvancedOperationVersionNumber;
+
+//! Project version string for AdvancedOperation.
+FOUNDATION_EXPORT const unsigned char AdvancedOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdvancedOperation/PublicHeader.h>
+
+
