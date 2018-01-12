e17a1e8a046174dce052ca208b440ffc498f05b0
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-01-12 10:38:34 +0100

    SPM support

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AdvancedOperation",
+    products: [
+        .library(name: "AdvancedOperation", targets: ["AdvancedOperation"])
+    ],
+    targets: [
+        .target(name: "AdvancedOperation", path: "Sources")
+    ]
+)
