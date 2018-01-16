b3079a5df04ed7717e56e5f25b17c118489d49d5
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-01-16 10:59:36 +0100

    added SPM tests

--- Package.swift
+++ Package.swift
@@ -4,11 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "AdvancedOperation",
-    products: [
-        .library(name: "AdvancedOperation", targets: ["AdvancedOperation"])
-    ],
-    targets: [
-        .target(name: "AdvancedOperation", path: "Sources")
-    ]
+  name: "AdvancedOperation",
+  products: [
+    .library(name: "AdvancedOperation", targets: ["AdvancedOperation"])
+  ],
+  targets: [
+    .target(name: "AdvancedOperation", path: "Sources"),
+    .testTarget(name: "AdvancedOperationTests", dependencies: ["AdvancedOperation"], path: "Tests")
+  ],
+  swiftLanguageVersions: [4]
 )
