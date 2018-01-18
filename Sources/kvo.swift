df63fbb689b95e3dbba4768a9e1fe77ecd5588b5
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-01-18 18:03:50 +0100

    kvo ideas

--- Sources/kvo.swift
+++ Sources/kvo.swift
@@ -23,6 +23,61 @@
 
 import Foundation
 
+/// Operation
+
+/**
+ The NSOperation class is key-value coding (KVC) and key-value observing (KVO) compliant for several of its properties. As needed, you can observe these properties to control other parts of your application. To observe the properties, use the following key paths:
+ isCancelled - read-only
+ isAsynchronous - read-only
+ isExecuting - read-only
+ isFinished - read-only
+ isReady - read-only
+ dependencies - read-only
+ queuePriority - readable and writable
+ completionBlock - readable and writable
+ **/
+
+class OperationKeyValueObserver: NSObject {
+  let operation: Operation
+  
+  private var _external = [OperationObserving]()
+  private var observers = [NSKeyValueObservation]()
+  
+  init(operation: Operation) {
+    self.operation = operation
+    super.init()
+    
+    self.setup()
+  }
+  
+  // https://www.objc.io/issues/7-foundation/key-value-coding-and-observing/
+  
+  private func setup() {
+    let cancel = observe(\.operation.isCancelled, options: [.prior, .old, .new]) { [weak self] (operation, change) in
+      
+      guard let `self` = self else { return }
+      
+      if change.isPrior {
+        guard let old = change.oldValue, old == true else { return }
+        for observer in self._external {
+          //willCancel
+        }
+        //will
+      } else {
+        guard let old = change.oldValue, let new = change.newValue, old == true, new == false else { return }
+        for observer in self._external {
+          //observer.operationDidCancel(operation: self.operation, withErrors: [])
+        }
+      }
+      
+      
+    }
+    
+    observers.append(contentsOf: [cancel])
+  }
+  
+}
+
 /*
  private lazy var stateObservers: [NSKeyValueObservation] = {
  //TODO: add the prior value and check if it's different from new
@@ -63,3 +118,15 @@ import Foundation
  return [cancelObserver, executeObserver, finishObserver]
  }()
  */
+
+
+/// OperationQueue
+
+/**
+ The NSOperationQueue class is key-value coding (KVC) and key-value observing (KVO) compliant. You can observe these properties as desired to control other parts of your application. To observe the properties, use the following key paths::
+ operations - read-only
+ operationCount - read-only
+ maxConcurrentOperationCount - readable and writable
+ suspended - readable and writable
+ name - readable and writable
+ **/
