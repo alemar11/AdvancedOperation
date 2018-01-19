0d39b96bc59dc436cc1de118257f971e8f4af248
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-01-19 17:49:09 +0100

    --wip-- [skip ci]

--- Sources/OperationObserving.swift
+++ Sources/OperationObserving.swift
@@ -27,12 +27,17 @@ import Foundation
 public protocol OperationObserving {
   /// An unique identifier
   var identifier: String { get set }
+
   /// Invoked immediately prior to the `Operation`'s `main()` method (it's started but not yet executed).
   func operationDidStart(operation: Operation)
+
   /// Invoked as an `Operation` finishes, along with any errors produced during execution.
+  /// - Note: An operation can finish without starting (i.e. if cancelled before its execution)
   func operationDidFinish(operation: Operation, withErrors errors: [Error])
+
   /// Invoked when an `Operation` is going to be cancelled, along with any errors produced during execution.
   func operationWillCancel(operation: Operation, withErrors errors: [Error]) //TODO: remove this func
+
   /// Invoked as an `Operation` is cancelled, along with any errors produced during execution.
   func operationDidCancel(operation: Operation, withErrors errors: [Error])
 }
