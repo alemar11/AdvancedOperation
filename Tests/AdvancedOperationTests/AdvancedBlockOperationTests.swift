ea408d2146cae8a4bc467c4c7837a0eb23a0fe5b
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-01-30 18:00:31 +0100

    added AdvancedBlockOperation

---
+++ Tests/AdvancedOperationTests/AdvancedBlockOperationTests.swift
@@ -0,0 +1,62 @@
+// 
+// AdvancedOperation
+//
+// Copyright © 2016-2018 Tinrobots.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import XCTest
+@testable import AdvancedOperation
+
+extension AdvancedBlockOperationTests {
+
+  static var allTests = [
+    ("testAsyncQueue", testAsyncQueue),
+  ]
+
+}
+class AdvancedBlockOperationTests: XCTestCase {
+        
+//    func testExample() {
+//      let operation = AdvancedBlockOperation(block: {
+//        print("=========================")
+//      })
+//      operation.start()
+//    }
+
+  func testAsyncQueue() {
+
+    let operation = AdvancedBlockOperation { complete in
+      //DispatchQueue.global().async {
+      DispatchQueue(label: "org.tinrobots.\(#function)", attributes: .concurrent).async {
+        sleep(1)
+        sleep(2)
+        complete()
+      }
+    }
+
+    let expectation1 = expectation(description: "\(#function)\(#line)")
+    operation.addCompletionBlock { expectation1.fulfill() }
+    operation.start()
+
+    waitForExpectations(timeout: 4)
+    XCTAssertOperationFinished(operation: operation)
+  }
+    
+}
