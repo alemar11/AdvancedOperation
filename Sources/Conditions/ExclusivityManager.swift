e1a65ac8eadd36c679da9d74b05457c05946fb81
Alessandro Marzoli <alessandromarzoli@gmail.com>
2018-09-30 18:38:00 +0200

    removed deinit

--- Sources/Conditions/ExclusivityManager.swift
+++ Sources/Conditions/ExclusivityManager.swift
@@ -103,20 +103,4 @@ final public class ExclusivityManager {
     }
   }
 
-  deinit {
-    queue.sync {
-      _operations.forEach { _, operations in
-        let error = AdvancedOperationError.executionCancelled(message: "The operation has been cancelled because the ExclusivityManager holding that operation has been deallocated.")
-        _ = operations.map {
-          if let advancedOperation = $0 as? AdvancedOperation {
-            advancedOperation.cancel(error: error)
-          } else {
-            $0.cancel()
-          }
-        }
-      }
-
-      _operations.removeAll()
-    }
-  }
 }
