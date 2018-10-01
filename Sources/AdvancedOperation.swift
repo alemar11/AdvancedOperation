87fcace3e99d0b03cbdf105e557d0d91093637c9
Alessandro Marzoli <alessandromarzoli@gmail.com>
2018-10-01 21:07:11 +0200

    chores

--- Sources/AdvancedOperation.swift
+++ Sources/AdvancedOperation.swift
@@ -22,57 +22,56 @@
 // SOFTWARE.
 
 import Foundation
-import os.log
 
 /// An advanced subclass of `Operation`.
 open class AdvancedOperation: Operation {
-
+  
   // MARK: - State
-
+  
   public final override var isReady: Bool {
     switch state {
-
+      
     case .pending:
       //      if isCancelled {
       //        return true
       //      }
-
+      
       if super.isReady {
         evaluateConditions()
         return false
       }
-
+      
       return false // Until conditions have been evaluated
-
+      
     case .ready:
       return super.isReady
-
+      
     default:
       return false
     }
-
+    
   }
-
+  
   public final override var isExecuting: Bool { return state == .executing }
-
+  
   public final override var isFinished: Bool { return state == .finished }
-
+  
   public final override var isCancelled: Bool { return lock.synchronized { return _cancelled && state != .evaluating } }
-
+  
   internal final var isCancelling: Bool { return lock.synchronized { return _cancelling } }
-
+  
   // MARK: - OperationState
-
+  
   @objc
   internal enum OperationState: Int, CustomDebugStringConvertible {
-
+    
     case ready
     case pending
     case evaluating
     case executing
     case finishing
     case finished
-
+    
     func canTransition(to state: OperationState) -> Bool {
       switch (self, state) {
       case (.ready, .executing):
@@ -93,7 +92,7 @@ open class AdvancedOperation: Operation {
         return false
       }
     }
-
+    
     var debugDescription: String {
       switch self {
       case .ready:
@@ -110,42 +109,42 @@ open class AdvancedOperation: Operation {
         return "finished"
       }
     }
-
+    
   }
-
+  
   // MARK: - Properties
-
+  
   /// Errors generated during the execution.
   public private(set) var errors: [Error] {
     get {
-     return lock.synchronized { return _errors }
+      return lock.synchronized { return _errors }
     }
     set {
       lock.synchronized { _errors = newValue }
     }
   }
-
+  
   /// Returns `true` if the `AdvancedOperation` failed due to errors.
   public var failed: Bool { return lock.synchronized { !errors.isEmpty } }
-
+  
   /// A lock to guard reads and writes to the `_state` property
   private let lock = NSRecursiveLock()
-
+  
   /// Private backing stored property for `state`.
   private var _state: OperationState = .ready
-
+  
   /// Returns `true` if the `AdvancedOperation` is finishing.
   private var _finishing = false
-
+  
   /// Returns `true` if the `AdvancedOperation` is cancelling.
   private var _cancelling = false
-
+  
   /// Returns `true` if the `AdvancedOperation` is cancelled.
   private var _cancelled = false
-
+  
   /// Errors generated during the execution.
   private var _errors = [Error]()
-
+  
   /// The state of the operation.
   @objc dynamic
   internal var state: OperationState {
@@ -157,7 +156,7 @@ open class AdvancedOperation: Operation {
       }
     }
   }
-
+  
   open override class func keyPathsForValuesAffectingValue(forKey key: String) -> Set<String> {
     switch key {
     case #keyPath(Operation.isReady),
@@ -168,61 +167,61 @@ open class AdvancedOperation: Operation {
       return super.keyPathsForValuesAffectingValue(forKey: key)
     }
   }
-
+  
   // MARK: - Life Cycle
-
+  
   deinit {
     for dependency in dependencies {
       removeDependency(dependency)
     }
   }
-
+  
   // MARK: - Observers
-
+  
   private(set) var observers = [OperationObservingType]()
-
+  
   // MARK: - Execution
-
+  
   public final override func start() {
-
+    
     // Do not start if it's finishing
     guard state != .finishing else {
       return
     }
-
+    
     // Bail out early if cancelled or if there are some errors.
     guard !failed && !isCancelled else {
       finish() // fires KVO
       return
     }
-
+    
     let canBeExecuted = lock.synchronized { () -> Bool in
       guard isReady else { return false }
-
+      
       guard !isExecuting else { return false }
-
+      
       state = .executing // recursive lock
       return true
     }
-
+    
     guard canBeExecuted else { return }
-
+    
     willExecute()
     main()
   }
-
+  
   open override func main() {
     fatalError("\(type(of: self)) must override `main()`.")
   }
-
+  
   public func cancel(error: Error? = nil) {
     _cancel(error: error)
   }
-
+  
   open override func cancel() {
     _cancel()
   }
-
+  
   private func _cancel(error: Error? = nil) {
     let canBeCancelled = lock.synchronized { () -> Bool in
       guard !_finishing && !isFinished else { return false }
@@ -230,17 +229,17 @@ open class AdvancedOperation: Operation {
       _cancelling = true
       return _cancelling
     }
-
+    
     guard canBeCancelled else { return }
-
+    
     var localErrors = errors
     if let error = error {
       localErrors.append(error)
     }
-
+    
     willChangeValue(forKey: #keyPath(AdvancedOperation.isCancelled))
     willCancel(errors: localErrors) // observers
-
+    
     lock.synchronized {
       if let error = error {
         self.errors.append(error)
@@ -251,113 +250,113 @@ open class AdvancedOperation: Operation {
       _cancelled = true
       _cancelling = false
     }
-
+    
     didCancel(errors: errors) // observers
     didChangeValue(forKey: #keyPath(AdvancedOperation.isCancelled))
-
+    
     super.cancel() // fires isReady KVO
   }
-
+  
   public final func finish(errors: [Error] = []) {
     let canBeFinished = lock.synchronized { () -> Bool in
       guard _state.canTransition(to: .finishing) else { return false }
       _state = .finishing
-
+      
       if !_finishing {
         _finishing = true
         return true
       }
       return false
     }
-
+    
     guard canBeFinished else { return }
-
+    
     let updatedErrors = lock.synchronized { () -> [Error] in
       self.errors.append(contentsOf: errors)
       return self.errors
     }
-
+    
     willFinish(errors: updatedErrors)
     state = .finished
     didFinish(errors: updatedErrors)
     lock.synchronized { _finishing = false }
   }
-
+  
   // MARK: - Subclass
-
+  
   /// Subclass this method to know when the operation will start executing.
   open func operationWillExecute() { }
-
+  
   /// Subclass this method to know when the operation has produced another `Operation`.
   open func operationDidProduceOperation(_ operation: Operation) { }
-
+  
   /// Subclass this method to know when the operation will be cancelled.
   open func operationWillCancel(errors: [Error]) { }
-
+  
   /// Subclass this method to know when the operation has been cancelled.
   open func operationDidCancel(errors: [Error]) { }
-
+  
   /// Subclass this method to know when the operation will finish its execution.
   open func operationWillFinish(errors: [Error]) { }
-
+  
   /// Subclass this method to know when the operation has finished executing.
   open func operationDidFinish(errors: [Error]) { }
-
+  
   // MARK: - Produced Operations
-
+  
   /// Produce another operation on the same `AdvancedOperationQueue` that this instance is on.
   ///
   /// - Parameter operation: an `Operation` instance.
   final func produceOperation(_ operation: Operation) {
     didProduceOperation(operation)
   }
-
+  
   // MARK: - Dependencies
-
+  
   open override func addDependency(_ operation: Operation) {
     assert(!isExecuting, "Dependencies cannot be modified after execution has begun.")
-
+    
     super.addDependency(operation)
   }
-
+  
   // MARK: - Conditions
-
+  
   public private(set) var conditions = [OperationCondition]()
-
+  
   /// Indicate to the operation running on a `AdvancedOperationQueue` that it can proceed with evaluating conditions (if it's not cancelled or finished).
   internal func willEnqueue() {
     guard !isCancelled else { return } // if it's cancelled, there's no point in evaluating the conditions
-
+    
     let canBeEnqueued = lock.synchronized { () -> Bool in
       return state.canTransition(to: .pending)
     }
-
+    
     guard canBeEnqueued else { return }
-
+    
     state = .pending
   }
-
+  
   public func addCondition(_ condition: OperationCondition) {
     assert(state == .ready || state == .pending, "Cannot add conditions if the operation is \(state).")
-
+    
     conditions.append(condition)
   }
-
+  
   private func evaluateConditions() {
     let canBeEvaluated = lock.synchronized { () -> Bool in
       guard state.canTransition(to: .evaluating) else { return false }
       state = .evaluating
       return true
     }
-
+    
     guard canBeEvaluated else { return }
-
+    
     type(of: self).evaluate(conditions, operation: self) { [weak self] errors in
       self?.errors.append(contentsOf: errors)
       self?.state = .ready
     }
   }
-
+  
 }
 
 // MARK: - Observers
@@ -369,69 +368,69 @@ extension AdvancedOperation {
   /// - Requires: `self must not have started.
   public func addObserver(_ observer: OperationObservingType) {
     assert(!isExecuting, "Cannot modify observers after execution has begun.")
-
+    
     observers.append(observer)
   }
-
+  
   internal var willExecuteObservers: [OperationWillExecuteObserving] {
     return observers.compactMap { $0 as? OperationWillExecuteObserving }
   }
-
+  
   internal var didProduceOperationObservers: [OperationDidProduceOperationObserving] {
     return observers.compactMap { $0 as? OperationDidProduceOperationObserving }
   }
-
+  
   internal var willCancelObservers: [OperationWillCancelObserving] {
     return observers.compactMap { $0 as? OperationWillCancelObserving }
   }
-
+  
   internal var didCancelObservers: [OperationDidCancelObserving] {
     return observers.compactMap { $0 as? OperationDidCancelObserving }
   }
-
+  
   internal var willFinishObservers: [OperationWillFinishObserving] {
     return observers.compactMap { $0 as? OperationWillFinishObserving }
   }
-
+  
   internal var didFinishObservers: [OperationDidFinishObserving] {
     return observers.compactMap { $0 as? OperationDidFinishObserving }
   }
-
+  
   private func willExecute() {
     operationWillExecute()
     for observer in willExecuteObservers {
       observer.operationWillExecute(operation: self)
     }
   }
-
+  
   private func didProduceOperation(_ operation: Operation) {
     operationDidProduceOperation(operation)
     for observer in didProduceOperationObservers {
       observer.operation(operation: self, didProduce: operation)
     }
   }
-
+  
   private func willFinish(errors: [Error]) {
     operationWillFinish(errors: errors)
     for observer in willFinishObservers {
       observer.operationWillFinish(operation: self, withErrors: errors)
     }
   }
-
+  
   private func didFinish(errors: [Error]) {
     operationDidFinish(errors: errors)
     for observer in didFinishObservers {
       observer.operationDidFinish(operation: self, withErrors: errors)
     }
   }
-
+  
   private func willCancel(errors: [Error]) {
     operationWillCancel(errors: errors)
     for observer in willCancelObservers {
       observer.operationWillCancel(operation: self, withErrors: errors)
     }
   }
-
+  
   private func didCancel(errors: [Error]) {
     operationDidCancel(errors: errors)
     for observer in didCancelObservers {
@@ -443,12 +442,12 @@ extension AdvancedOperation {
 // MARK: - Condition Evaluation
 
 extension AdvancedOperation {
-
+  
   private static func evaluate(_ conditions: [OperationCondition], operation: AdvancedOperation, completion: @escaping ([Error]) -> Void) {
     let conditionGroup = DispatchGroup()
     var results = [OperationConditionResult?](repeating: nil, count: conditions.count)
     let lock = NSLock()
-
+    
     // Even if an operation is cancelled, the conditions are evaluated nonetheless.
     for (index, condition) in conditions.enumerated() {
       conditionGroup.enter()
@@ -456,11 +455,11 @@ extension AdvancedOperation {
         lock.synchronized {
           results[index] = result
         }
-
+        
         conditionGroup.leave()
       }
     }
-
+    
     conditionGroup.notify(queue: DispatchQueue.global()) {
       // Aggregate all the occurred errors.
       let errors = results.compactMap { (result) -> [Error]? in
@@ -470,16 +469,16 @@ extension AdvancedOperation {
         default:
           return nil
         }
-      }.flatMap { $0 }
-
-//      if operation.isCancelled {
-//        var aggregatedErrors = operation.errors
-//        let error = AdvancedOperationError.executionCancelled(message: "Operation cancelled while evaluating its conditions.")
-//        errors.append(contentsOf: aggregatedErrors)
-//      }
-
+        }.flatMap { $0 }
+      
+      //      if operation.isCancelled {
+      //        var aggregatedErrors = operation.errors
+      //        let error = AdvancedOperationError.executionCancelled(message: "Operation cancelled while evaluating its conditions.")
+      //        errors.append(contentsOf: aggregatedErrors)
+      //      }
+      
       completion(errors)
     }
   }
-
+  
 }
--- Sources/AdvancedOperationQueue.swift
+++ Sources/AdvancedOperationQueue.swift
@@ -23,19 +23,17 @@
 
 import Foundation
 
-//public protocol AdvancedOperationQueueDelegate: class {
-//  func operationQueue(operationQueue: AdvancedOperationQueue, willAddOperation operation: Operation)
-//  func operationQueue(operationQueue: AdvancedOperationQueue, didAddOperation operation: Operation)
-//
-//  func operationQueue(operationQueue: AdvancedOperationQueue, operationWillExecute operation: Operation)
-//  func operationQueue(operationQueue: AdvancedOperationQueue, operationDidFinish operation: Operation, withErrors errors: [Error])
-//  func operationQueue(operationQueue: AdvancedOperationQueue, operationDidCancel operation: Operation, withErrors errors: [Error])
-//
-//  func operationQueue(operationQueue: AdvancedOperationQueue, operationWillFinish operation: Operation, withErrors errors: [Error])
-//  func operationQueue(operationQueue: AdvancedOperationQueue, operationWillCancel operation: Operation, withErrors errors: [Error])
-//}
-
-public protocol AdvancedOperationQueueDelegate: class, OperationQueueObserving { }
+public protocol AdvancedOperationQueueDelegate: class {
+  func operationQueue(operationQueue: AdvancedOperationQueue, willAddOperation operation: Operation)
+  func operationQueue(operationQueue: AdvancedOperationQueue, didAddOperation operation: Operation)
+  
+  func operationQueue(operationQueue: AdvancedOperationQueue, operationWillExecute operation: Operation)
+  func operationQueue(operationQueue: AdvancedOperationQueue, operationDidFinish operation: Operation, withErrors errors: [Error])
+  func operationQueue(operationQueue: AdvancedOperationQueue, operationDidCancel operation: Operation, withErrors errors: [Error])
+  
+  func operationQueue(operationQueue: AdvancedOperationQueue, operationWillFinish operation: Operation, withErrors errors: [Error])
+  func operationQueue(operationQueue: AdvancedOperationQueue, operationWillCancel operation: Operation, withErrors errors: [Error])
+}
 
 public extension AdvancedOperationQueueDelegate {
   func operationQueue(operationQueue: AdvancedOperationQueue, willAddOperation operation: Operation) {}
@@ -44,75 +42,66 @@ public extension AdvancedOperationQueueDelegate {
 
 /// `AdvancedOperationQueue` is an `OperationQueue` subclass that implements a large number of "extra features" related to the `Operation` class.
 open class AdvancedOperationQueue: OperationQueue {
-
+  
   public weak var delegate: AdvancedOperationQueueDelegate?
-
+  
   private let exclusivityManager: ExclusivityManager
-
+  
+  private let lock = NSRecursiveLock()
+  
   public init(exclusivityManager: ExclusivityManager = .sharedInstance) {
     self.exclusivityManager = exclusivityManager
   }
-
-  private let lock = NSRecursiveLock()
-
-  private(set) var observers = [OperationQueueObservingType]()
-
+  
   // swiftlint:disable:next cyclomatic_complexity
   open override func addOperation (_ operation: Operation) {
     lock.synchronized {
-
       if let operation = operation as? AdvancedOperation { /// AdvancedOperation
-
+        
         let observer = BlockObserver(
           willExecute: { [weak self] (operation) in
             guard let self = self else { return }
-
+            
             self.delegate?.operationQueue(operationQueue: self, operationWillExecute: operation)
-            self.willExecute(operation: operation)
-
+            
           }, didProduce: { [weak self] in
             guard let self = self else { return }
-
+            
             self.addOperation($1)
-
+            
           }, willCancel: { [weak self] (operation, errors) in
             guard let self = self else { return }
-
+            
             self.delegate?.operationQueue(operationQueue: self, operationWillCancel: operation, withErrors: errors)
-            self.willCancel(operation: operation, withErrors: errors)
-
+            
           }, didCancel: { [weak self] (operation, errors) in
             guard let self = self else { return }
-
+            
             self.delegate?.operationQueue(operationQueue: self, operationDidCancel: operation, withErrors: errors)
-            self.didCancel(operation: operation, withErrors: errors)
-
+            
           }, willFinish: { [weak self] (operation, errors) in
             guard let self = self else { return }
-
+            
             self.delegate?.operationQueue(operationQueue: self, operationWillFinish: operation, withErrors: errors)
-            self.willFinish(operation: operation, withErrors: errors)
-
+            
           }, didFinish: { [weak self] (operation, errors) in
             guard let self = self else { return }
-
+            
             self.delegate?.operationQueue(operationQueue: self, operationDidFinish: operation, withErrors: errors)
-            self.didFinish(operation: operation, withErrors: errors)
-
           }
         )
-
+        
         operation.addObserver(observer)
-
+        
         // Conditions
         if !operation.conditions.isEmpty {
-
+          
           let dependencies = operation.conditions.compactMap { $0.dependency(for: operation) }
           for dependency in dependencies {
             self.addOperation(dependency)
             operation.addDependency(dependency)
           }
-
+          
           let mutuallyExclusiveConditions = operation.conditions.filter { $0.mutuallyExclusivityMode != .disabled }
           if !mutuallyExclusiveConditions.isEmpty {
             for condition in mutuallyExclusiveConditions {
@@ -121,37 +110,33 @@ open class AdvancedOperationQueue: OperationQueue {
               exclusivityManager.addOperation(operation, category: category, cancellable: cancellable)
             }
           }
-
+          
           operation.willEnqueue()
         }
-
+        
       } else { /// Operation
-
+        
         // For regular `Operation`s, we'll manually call out to the queue's delegate we don't want
         // to just capture "operation" because that would lead to the operation strongly referencing itself and that's the pure definition of a memory leak.
         operation.addCompletionBlock(asEndingBlock: false) { [weak self, weak operation] in
           guard let queue = self, let operation = operation else { return }
-
+          
           queue.delegate?.operationQueue(operationQueue: queue, operationDidFinish: operation, withErrors: [])
-          queue.didFinish(operation: operation, withErrors: [])
         }
       }
-
+      
       delegate?.operationQueue(operationQueue: self, willAddOperation: operation)
-      willAdd(operation: operation)
       super.addOperation(operation)
       delegate?.operationQueue(operationQueue: self, didAddOperation: operation)
-      didAdd(operation: operation)
-
     }
   }
-
+  
   open override func addOperations(_ operations: [Operation], waitUntilFinished wait: Bool) {
-
+    
     for operation in operations {
       addOperation(operation)
     }
-
+    
     if wait {
       // waitUntilAllOperationsAreFinished()
       for operation in operations {
@@ -159,75 +144,5 @@ open class AdvancedOperationQueue: OperationQueue {
       }
     }
   }
-
-}
-
-// MARK: - Observers
-
-extension AdvancedOperationQueue {
-
-  public func addObserver(_ observer: OperationQueueObserving) {
-    assert(isSuspended, "Cannot modify observers after execution has begun.")
-
-    observers.append(observer)
-  }
-
-  internal var addOperationObservers: [OperationQueueAddOperationObserving] {
-    return observers.compactMap { $0 as? OperationQueueAddOperationObserving }
-  }
-
-  internal var willExecuteOperationObservers: [OperationQueueWillExecuteOperationObserving] {
-    return observers.compactMap { $0 as? OperationQueueWillExecuteOperationObserving }
-  }
-
-  internal var cancelOperationObservers: [OperationQueueCancelOperationObserving] {
-    return observers.compactMap { $0 as? OperationQueueCancelOperationObserving }
-  }
-
-  internal var finishOperationObservers: [OperationQueueFinishOperationObserving] {
-    return observers.compactMap { $0 as? OperationQueueFinishOperationObserving }
-  }
-
-  func willAdd(operation: Operation) {
-    for observer in addOperationObservers {
-      observer.operationQueue(operationQueue: self, willAddOperation: operation)
-    }
-  }
-
-  func didAdd(operation: Operation) {
-    for observer in addOperationObservers {
-      observer.operationQueue(operationQueue: self, didAddOperation: operation)
-    }
-  }
-
-  func willExecute(operation: Operation) {
-    for observer in willExecuteOperationObservers {
-      observer.operationQueue(operationQueue: self, operationWillExecute: operation)
-    }
-  }
-
-  func willCancel(operation: Operation, withErrors errors: [Error]) {
-    for observer in cancelOperationObservers {
-      observer.operationQueue(operationQueue: self, operationWillCancel: operation, withErrors: errors)
-    }
-  }
-
-  func didCancel(operation: Operation, withErrors errors: [Error]) {
-    for observer in cancelOperationObservers {
-      observer.operationQueue(operationQueue: self, operationDidCancel: operation, withErrors: errors)
-    }
-  }
-
-  func willFinish(operation: Operation, withErrors errors: [Error]) {
-    for observer in finishOperationObservers {
-      observer.operationQueue(operationQueue: self, operationWillFinish: operation, withErrors: errors)
-    }
-  }
-
-  func didFinish(operation: Operation, withErrors errors: [Error]) {
-    for observer in finishOperationObservers {
-      observer.operationQueue(operationQueue: self, operationDidFinish: operation, withErrors: errors)
-    }
-  }
-
+  
 }
