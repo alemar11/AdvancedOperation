69adf60e915d7e15fcccb08869069c5fde5b676c
Alessandro Marzoli <alessandromarzoli@gmail.com>
2018-10-28 20:51:03 +0100

    test

--- Tests/AdvancedOperationTests/AdvancedOperationQueueTests.swift
+++ Tests/AdvancedOperationTests/AdvancedOperationQueueTests.swift
@@ -604,20 +604,22 @@ final class AdvancedOperationQueueTests: XCTestCase {
 
   func testOperationRetainedUnitisExecuted() {
     let expectation1 = self.expectation(description: "test")
-    let manager = ExclusivityManager()
     let operation1 = AdvancedBlockOperation { complete in complete([]) }
     operation1.completionBlock = {
       expectation1.fulfill()
     }
 
-    var queue1: AdvancedOperationQueue? = AdvancedOperationQueue(exclusivityManager: manager)
+    var queue1: AdvancedOperationQueue? = AdvancedOperationQueue(exclusivityManager: ExclusivityManager())
     weak var weakQueue1 = queue1
     queue1!.isSuspended = true
     queue1!.addOperation(operation1)
 
-    queue1 = nil
+    autoreleasepool {
+      queue1 = nil
+    }
+
     // https://stackoverflow.com/questions/24415246/are-nsoperationqueues-retained-automatically-while-running
-    XCTAssertNotNil(weakQueue1)
+    XCTAssertNotNil(weakQueue1) // a queue is not released until its operation are not finished
     weakQueue1!.isSuspended = false
     waitForExpectations(timeout: 10)
     XCTAssertNil(weakQueue1)
