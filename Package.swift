d78b97791310ca82006dab81dc0b9156cd2dd8c2
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-07-03 21:33:21 +0200

    Added platforms support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "AdvancedOperation",
+  platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
   products: [
     .library(name: "AdvancedOperation", targets: ["AdvancedOperation"])
   ],
