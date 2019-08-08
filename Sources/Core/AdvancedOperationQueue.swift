3f78c53dbc8d32754f8af0a7a84454edcb2fb987
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-08-08 12:14:36 +0200

    cleaning

--- Sources/Core/AdvancedOperationQueue.swift
+++ Sources/Core/AdvancedOperationQueue.swift
@@ -23,12 +23,12 @@
 
 import Foundation
 
-/// `AdvancedOperationQueue` is an `OperationQueue` subclass that implements a large number of "extra features" related to the `Operation` class.
+/// `AdvancedOperationQueue` is an `OperationQueue` subclass that implements "extra features" related to the `AdvancedOperation` class.
 open class AdvancedOperationQueue: OperationQueue {
   /// Keeps track of every mutual exclusivity conditions defined in the operations running on this queue.
   internal lazy var exclusivityManager: ExclusivityManager = {
     let qos: DispatchQoS
-
+    
     switch self.qualityOfService {
     case .userInteractive:
       qos = .userInteractive
@@ -43,94 +43,48 @@ open class AdvancedOperationQueue: OperationQueue {
     @unknown default:
       qos = .default
     }
-
+    
     let manager = ExclusivityManager(qos: qos)
-
+    
     return manager
   }()
-
+  
   private let lock = UnfairLock()
-
+  
   open override func addOperation(_ operation: Operation) {
     lock.synchronized {
       let preparedOperations = prepareOperation(operation)
-
+      
       preparedOperations.forEach {
         super.addOperation($0)
       }
     }
   }
-
-  open override func addOperation(_ block: @escaping () -> Void) {
-    lock.synchronized {
-      let blockOperation = BlockOperation(block: block)
-      let preparedOperations = self.prepareOperation(blockOperation)
-
-      preparedOperations.forEach {
-        super.addOperation($0)
-      }
-    }
-  }
-
+  
   open override func addOperations(_ operations: [Operation], waitUntilFinished wait: Bool) {
     lock.synchronized {
       var preparedOperations = [Operation]()
       operations.forEach {
         preparedOperations.append(contentsOf: prepareOperation($0))
       }
-
-     _addOperations(preparedOperations, waitUntilFinished: wait)
+      super.addOperations(preparedOperations, waitUntilFinished: wait)
     }
-  }
-
-  private func _addOperations(_ operations: [Operation], waitUntilFinished wait: Bool) {
-     super.addOperations(operations, waitUntilFinished: wait)
   }
 }
 
 extension AdvancedOperationQueue {
-  /// Adds AdvancedOperationQueueDelegate, Conditions and ExclusivityManager features support.
+  /// Adds Conditions and ExclusivityManager features support.
   /// Returns the passed operation with a list of related operations to support all the available features.
   private func prepareOperation(_ operation: Operation) -> [Operation] {
     var operations = [Operation]()
-
+    
     // swiftlint:disable:next cyclomatic_complexity
     func decorateOperation(_ operation: Operation) {
       if let operation = operation as? AdvancedOperation { /// AdvancedOperation
-//        let observer = BlockObserver(
-//          willExecute: { [weak self] (operation) in
-//            guard let self = self else { return }
-//
-//            self.delegate?.operationQueue(operationQueue: self, operationWillExecute: operation)
-//          }, didProduce: { [weak self] (operation, producedOperation) in
-//            guard let self = self else { return }
-//
-//            self.addOperation(producedOperation)
-//          }, willCancel: { [weak self] (operation, error) in
-//            guard let self = self else { return }
-//
-//            self.delegate?.operationQueue(operationQueue: self, operationWillCancel: operation, withError: error)
-//          }, didCancel: { [weak self] (operation, error) in
-//            guard let self = self else { return }
-//
-//            self.delegate?.operationQueue(operationQueue: self, operationDidCancel: operation, withError: error)
-//          }, willFinish: { [weak self] (operation, error) in
-//            guard let self = self else { return }
-//
-//            self.delegate?.operationQueue(operationQueue: self, operationWillFinish: operation, withError: error)
-//          }, didFinish: { [weak self] (operation, error) in
-//            guard let self = self else { return }
-//
-//            self.delegate?.operationQueue(operationQueue: self, operationDidFinish: operation, withError: error)
-//          }
-//        )
-//
-//        operation.addObserver(observer)
-
         if let evaluator = operation.makeConditionsEvaluator() {
           decorateOperation(evaluator)
         }
-
+        
         for mutualExclusivityCondition in operation.conditions.compactMap ({ $0 as? MutualExclusivityCondition }) {
           switch mutualExclusivityCondition.mode {
           case .cancel(identifier: let identifier):
@@ -139,18 +93,10 @@ extension AdvancedOperationQueue {
             self.exclusivityManager.addOperation(operation, category: identifier, cancellable: false)
           }
         }
-      } else { /// Operation
-//        operation.addCompletionBlock(asEndingBlock: false) { [weak self, weak operation] in
-//          guard let self = self, let operation = operation else {
-//            return
-//          }
-//
-//          self.delegate?.operationQueue(operationQueue: self, operationDidFinish: operation, withError: nil)
-//        }
       }
       operations.append(operation)
     }
-
+    
     decorateOperation(operation)
     return operations
   }
