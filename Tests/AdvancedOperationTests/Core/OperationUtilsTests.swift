74460a1bc8e93f68aeff2ef3a8b32bcd3bcb3936
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-02-16 21:31:09 +0100

    chores

--- Tests/AdvancedOperationTests/Core/OperationUtilsTests.swift
+++ Tests/AdvancedOperationTests/Core/OperationUtilsTests.swift
@@ -112,11 +112,18 @@ final class OperationUtilsTests: XCTestCase {
     XCTAssertEqual(operation4.dependencies.count, 0)
   }
 
+  func testStress() {
+    (1...1000).forEach { (i) in
+      print(i)
+      testHasSomeDependenciesFailed()
+    }
+  }
+
   func testHasSomeDependenciesFailed() {
     let operation1 = BlockOperation()
     let operation2 = BlockOperation()
     let operation3 = FailingOperation()
-    let operation4 = AsyncBlockOperation() { $0()}
+    let operation4 = AsyncBlockOperation() { $0() }
 
     operation3.addDependencies(operation1, operation2)
     operation4.addDependency(operation3)
@@ -127,7 +134,8 @@ final class OperationUtilsTests: XCTestCase {
     queue.addOperations([operation1, operation2, operation3, operation4], waitUntilFinished: true)
     XCTAssertFalse(operation3.hasSomeFailedDependencies)
     XCTAssertTrue(operation4.hasSomeFailedDependencies)
-    XCTAssertEqual(operation3.error, .errorOne)  }
+    XCTAssertEqual(operation3.error, .errorOne)
+  }
 
   func testHasSomeDependenciesCancelled() {
     let operation1 = BlockOperation()
