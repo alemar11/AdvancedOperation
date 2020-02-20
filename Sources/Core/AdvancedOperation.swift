a5ad35c12c26104be17d9bdc9fb070033ef8f95f
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-02-20 15:22:48 +0100

    tech note added

--- Sources/Core/AdvancedOperation.swift
+++ Sources/Core/AdvancedOperation.swift
@@ -28,3 +28,7 @@ internal let identifier = "org.tinrobots.AdvancedOperation"
 // Quality of Service Inference and Promotion
 //
 // https://developer.apple.com/library/archive/documentation/Performance/Conceptual/EnergyGuide-iOS/PrioritizeWorkWithQoS.html#//apple_ref/doc/uid/TP40015243-CH39
+//
+// Simple and Reliable Threading with NSOperation
+//
+// https://developer.apple.com/library/archive/technotes/tn2109/_index.html#//apple_ref/doc/uid/DTS40010274-CH1-SUBSECTION11
