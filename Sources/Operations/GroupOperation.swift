c4552f3b48cda160c501703325b26262cfc5f166
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-10-01 18:01:12 +0200

    test

--- Sources/Operations/GroupOperation.swift
+++ Sources/Operations/GroupOperation.swift
@@ -147,6 +147,7 @@ open class GroupOperation: AsynchronousOperation {
 
             if #available(iOS 13.0, iOSApplicationExtension 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *) {
               // if the cancelled operation is executing, the queue progress will be updated when the operation finishes
+              
               if !operation.isExecuting && self.operationQueue.progress.totalUnitCount > 0 {
                 self.operationQueue.progress.totalUnitCount -= 1
               }
