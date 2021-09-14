78dd567fa27b4dc185fca15f2b28a25b7b2e9e89
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-09-14 14:37:40 +0200

    added TaskOperation to support async blocks

---
+++ Sources/Operations/TaskOperation.swift
@@ -0,0 +1,49 @@
+// TaskOperation
+
+import Foundation
+
+@available(swift 5.5)
+@available(iOS 15.0, iOSApplicationExtension 15.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, macOS 12, *)
+public final class TaskOperation: AsynchronousOperation {
+  public typealias Block =  @Sendable () async -> Void
+  
+  // MARK: - Private Properties
+  
+  private var block: Block
+  private var task: (Task<(), Never>)?
+  
+  // MARK: - Initializers
+  
+  /// The designated initializer.
+  ///
+  /// - Parameters:
+  ///   - block: The closure to run when the operation executes.
+  public init(block: @escaping Block) {
+    self.block = block
+    super.init()
+    self.name = "\(type(of: self))"
+  }
+  
+  // MARK: - Overrides
+  
+  public final override func main() {
+    guard !isCancelled else {
+      finish()
+      return
+    }
+    
+    task = Task {
+      print(Task.currentPriority)
+      if !Task.isCancelled {
+        await block()
+      }
+      finish()
+    }
+  }
+  
+  public override func cancel() {
+    task?.cancel()
+    super.cancel()
+  }
+}
+
