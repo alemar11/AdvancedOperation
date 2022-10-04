6af3dda8d85e885961859fbd35a1522cf1970536
Alessandro Marzoli <alessandromarzoli@gmail.com>
2022-10-04 17:51:57 +0200

    ideas

--- Sources/Operations/TaskOperation.swift
+++ Sources/Operations/TaskOperation.swift
@@ -7,12 +7,142 @@ import Foundation
 /// An `AsynchronousOperation` that supports Swift concurrency.
 @available(iOS 13.0, iOSApplicationExtension 13.0, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
 public final class TaskOperation: AsynchronousOperation {
-  public typealias Block =  @Sendable () async -> Void
+  public typealias Block = @Sendable () async -> Void
+  
+  // MARK: - Private Properties
+  
+  private var block: Block
+  private var task: (Task<(), Never>)?
+  
+  // MARK: - Initializers
+  
+  /// The designated initializer.
+  ///
+  /// - Parameters:
+  ///   - block: The closure to run when the operation executes.
+  public init(block: @escaping Block) {
+    self.block = block
+    super.init()
+    self.name = "\(type(of: self))"
+  }
+  
+  // MARK: - Overrides
+  
+  public final override func main() {
+    guard !isCancelled else {
+      finish()
+      return
+    }
+    
+    let priority = priority()
+    task = Task(priority: priority) {
+      if !Task.isCancelled {
+        await block()
+      }
+      finish()
+    }
+  }
+  
+  public override func cancel() {
+    task?.cancel()
+    super.cancel()
+  }
+  
+  private func priority() -> TaskPriority {
+    switch qualityOfService {
+      case .background:
+        return TaskPriority.background
+      case .default:
+        return TaskPriority.medium
+      case .userInitiated:
+        return TaskPriority.userInitiated
+      case .userInteractive:
+        return TaskPriority.high
+      case .utility:
+        return TaskPriority.utility
+      @unknown default:
+        return TaskPriority.medium
+    }
+  }
+}
+
+/// An `AsynchronousOperation` that supports Swift concurrency.
+@available(iOS 13.0, iOSApplicationExtension 13.0, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+public final class FailableTaskOperation: FailableAsynchronousOperation<Error> {
+  public typealias Block = @Sendable () async throws -> Void
+  
+  // MARK: - Private Properties
+  
+  private var block: Block
+  private var task: (Task<(), Never>)?
+  
+  // MARK: - Initializers
+  
+  /// The designated initializer.
+  ///
+  /// - Parameters:
+  ///   - block: The closure to run when the operation executes.
+  public init(block: @escaping Block) {
+    self.block = block
+    super.init()
+    self.name = "\(type(of: self))"
+  }
+  
+  // MARK: - Overrides
+  
+  public final override func main() {
+    guard !isCancelled else {
+      finish()
+      return
+    }
+    
+    let priority = priority()
+    task = Task(priority: priority) {
+      guard !Task.isCancelled else {
+        finish()
+        return
+      }
+      do {
+        try await block()
+        finish()
+      }  catch {
+        finish(with: error)
+      }
+    }
+  }
+  
+  public override func cancel() {
+    task?.cancel()
+    super.cancel()
+  }
+  
+  private func priority() -> TaskPriority {
+    switch qualityOfService {
+      case .background:
+        return TaskPriority.background
+      case .default:
+        return TaskPriority.medium
+      case .userInitiated:
+        return TaskPriority.userInitiated
+      case .userInteractive:
+        return TaskPriority.high
+      case .utility:
+        return TaskPriority.utility
+      @unknown default:
+        return TaskPriority.medium
+    }
+  }
+}
+
+/// An `AsynchronousOperation` that supports Swift concurrency.
+@available(iOS 13.0, iOSApplicationExtension 13.0, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+public final class ResultTaskOperation<Success, Failure>: ResultOperation<Success, Error> {
+  public typealias Block = @Sendable () async throws -> Success
 
   // MARK: - Private Properties
 
   private var block: Block
-  private var task: (Task<(), Never>)?
+  private var task: (Task<Void, Never>)?
 
   // MARK: - Initializers
 
@@ -36,10 +166,13 @@ public final class TaskOperation: AsynchronousOperation {
 
     let priority = priority()
     task = Task(priority: priority) {
-      if !Task.isCancelled {
-        await block()
+      do {
+        try Task.checkCancellation()
+        let result = try await block()
+        finish(with: .success(result))
+      } catch {
+        finish(with: .failure(error))
       }
-      finish()
     }
   }
 
@@ -65,5 +198,4 @@ public final class TaskOperation: AsynchronousOperation {
     }
   }
 }
-
 #endif
--- Tests/AdvancedOperationTests/Operations/TaskOperationTests.swift
+++ Tests/AdvancedOperationTests/Operations/TaskOperationTests.swift
@@ -55,3 +55,91 @@ final class TaskOperationTests: XCTestCase {
     wait(for: [finishExpectation], timeout: 2)
   }
 }
+
+
+@available(swift 5.5)
+@available(iOS 15.0, iOSApplicationExtension 15.0, macCatalyst 15.0, tvOS 15.0, watchOS 8.0, macOS 12, *)
+final class FailableTaskOperationTests: XCTestCase {
+  func testExecution() {
+    let expection1 = expectation(description: "\(#function)\(#line)")
+    @Sendable func test() async throws -> Void { expection1.fulfill() }
+    let operation = FailableTaskOperation {
+      XCTAssertEqual(Task.currentPriority, .medium)
+      try await test()
+    }
+    let finishExpectation = XCTKVOExpectation(keyPath: #keyPath(Operation.isFinished), object: operation, expectedValue: true)
+    operation.start()
+    wait(for: [expection1, finishExpectation], timeout: 2)
+    XCTAssertNil(operation.error)
+  }
+
+  func testFailedExecution() {
+    @Sendable func test() async throws -> Void {
+      throw NSError(domain: "Tests", code: 1, userInfo: nil)
+    }
+    let operation = FailableTaskOperation {
+      XCTAssertEqual(Task.currentPriority, .medium)
+      try await test()
+    }
+    let finishExpectation = XCTKVOExpectation(keyPath: #keyPath(Operation.isFinished), object: operation, expectedValue: true)
+    operation.start()
+    wait(for: [finishExpectation], timeout: 2)
+    XCTAssertNotNil(operation.error)
+  }
+
+  func testExecutionInQueue() {
+    let queue = OperationQueue()
+    queue.maxConcurrentOperationCount = 2
+    queue.isSuspended = true
+    let expection1 = expectation(description: "\(#function)\(#line)")
+    @Sendable func test() async throws -> Void { expection1.fulfill() }
+    let operation = FailableTaskOperation() {
+      XCTAssertEqual(Task.currentPriority, .userInitiated)
+      try await test()
+    }
+    operation.qualityOfService = .userInitiated
+    let finishExpectation = XCTKVOExpectation(keyPath: #keyPath(Operation.isFinished), object: operation, expectedValue: true)
+    queue.addOperation(operation)
+    queue.isSuspended = false
+    wait(for: [expection1, finishExpectation], timeout: 2)
+  }
+
+  func testFailedExecutionInQueue() {
+    let queue = OperationQueue()
+    queue.maxConcurrentOperationCount = 2
+    queue.isSuspended = true
+    @Sendable func test() async throws -> Void {
+      throw NSError(domain: "Tests", code: 1, userInfo: nil)
+    }
+    let operation = FailableTaskOperation() {
+      XCTAssertEqual(Task.currentPriority, .userInitiated)
+      try await test()
+    }
+    operation.qualityOfService = .userInitiated
+    let finishExpectation = XCTKVOExpectation(keyPath: #keyPath(Operation.isFinished), object: operation, expectedValue: true)
+    queue.addOperation(operation)
+    queue.isSuspended = false
+    wait(for: [finishExpectation], timeout: 2)
+    XCTAssertNotNil(operation.error)
+  }
+
+  func testCancel() {
+    let operation = FailableTaskOperation { XCTFail("The underlying task should be cancelled.") }
+    let finishExpectation = XCTKVOExpectation(keyPath: #keyPath(Operation.isFinished), object: operation, expectedValue: true)
+    operation.cancel()
+    operation.start()
+    wait(for: [finishExpectation], timeout: 2)
+  }
+
+  func testCancelInQueue() {
+    let queue = OperationQueue()
+    queue.maxConcurrentOperationCount = 2
+    queue.isSuspended = true
+    let operation = FailableTaskOperation { XCTFail("The underlying task should be cancelled.") }
+    let finishExpectation = XCTKVOExpectation(keyPath: #keyPath(Operation.isFinished), object: operation, expectedValue: true)
+    queue.addOperation(operation)
+    queue.cancelAllOperations()
+    queue.isSuspended = false
+    wait(for: [finishExpectation], timeout: 2)
+  }
+}
